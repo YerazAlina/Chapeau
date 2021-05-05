77a3cb5cd8d085d847fff5678bcae385bc6700d8
Yeraz Ayana <Yeraz.alina@outlook.com>
2021-05-05 19:44:01 +0200

    3

--- ChapeauOrderingSystem/ChapeauModel/Order.cs
+++ ChapeauOrderingSystem/ChapeauModel/Order.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ChapeauModel
 {
     public class Order
     {
+        //Class Order can contain a list of OrderItem-objects(and a Table instead of TableId, and a Employee instead of EmployeeId)
+        public List<OrderItem> orderedItems = new List<OrderItem>();
+
+        public int OrderNr { get; set; }
+        public int TableID { get; set; }
+        public int EmployeeID { get; set; }
+        public DateTime DateTime { get; set; }
+        public decimal Bill { get; set; }
     }
 }
---
+++ ChapeauOrderingSystem/ChapeauModel/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChapeauModel
+{
+    //Class OrderItem can contain a MenuItem instead of (int) MenuItemId
+    public enum MenuItem
+    { }
+
+    public enum Catogory
+    { Lunch, Dinner, Drinks }
+
+    public class OrderItem
+    {
+
+    }
+}
